0e35d89a714dd0011a963dbbe0abc788ceffb963
Arnaldo Pagani Junior <arnaldopaganijr@gmail.com>
2024-09-02 20:11:12 -0300

    Criada o Navbar menu e estilizado, criada as pages

--- front-end/src/components/nav-bar/navBar.jsx
+++ front-end/src/components/nav-bar/navBar.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navBar.css";
 import { assets } from "../../assets/assets";
 const NavBar = () => {
+    const [menu, setMenu] = useState("home")
+
     return (
-        <div className="navBar">
+        <div className="navbar">
             <img className="logo" src={assets.logo} />
             <ul className="navbar-menu">
-                <li>Home</li>
-                <li>Menu</li>
-                <li>mobile-app</li>
-                <li>Contact</li>
+                <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
+                <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>
+                <li onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-app</li>
+                <li onClick={() => setMenu("contact")} className={menu === "contact" ? "active" : ""}>Contact</li>
             </ul>
-            <div></div>
+            <div className="navbar-right">
+                <img src={assets.search_icon} />
+                <div className="navbar-search-icon">
+                    <img src={assets.basket_icon} alt="" />
+                    <div className="dot"></div>
+                </div>
+                <button>sing in</button>
+            </div>
         </div>
     );
 };
