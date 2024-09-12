aa1ece03143fd1932c3e2eccd4971e7c7d85cf15
Arnaldo Pagani Junior <arnaldopaganijr@gmail.com>
2024-09-12 19:19:25 -0300

    commitando as ultimas alterações

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import NavBar from "./components/nav-bar/navBar";
-import FoodItem from "./components/FoodItem/FoodItem";
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/home";
+import Cart from "./pages/Cart/cart"
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 const App = () => {
     return (
         <div className="app">
             <NavBar />
-            <FoodItem />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/placeOrder" element={<PlaceOrder />} />
+            </Routes>
         </div>
     );
 };
---
+++ front-end/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header-content">
+                <h2>Order your food</h2>
+                <p>Chose o caralho todo</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
